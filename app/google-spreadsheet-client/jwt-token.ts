0c1f7020598de4d55d143686fdd8d103d14b5058
jopijuco <jpjc@protonmail.com>
2023-11-17 19:08:23 +0000

    Config fixed to work with heroku

--- app/google-spreadsheet-client/jwt-token.ts
+++ app/google-spreadsheet-client/jwt-token.ts
@@ -3,5 +3,9 @@ import { JWT } from "google-auth-library";
 export const jwt = new JWT({
   email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
   key: process.env.GOOGLE_PRIVATE_KEY,
-  scopes: process.env.GOOGLE_API_SCOPE
+  scopes: [
+    "https://www.googleapis.com/auth/spreadsheets.readonly",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive.readonly",
+  ]
 });
