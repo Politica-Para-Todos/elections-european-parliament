4144012ba53efc840fab2b41dd262383b8e7b766
jopijuco <jpjc@protonmail.com>
2023-11-19 22:52:14 +0000

    Revert container deploy

--- next.config.js
+++ next.config.js
@@ -30,8 +30,7 @@ module.exports = {
       protocol: 'https',
       hostname: 'upload.wikimedia.org'
     }]
-  },
-  output: "standalone"
+  }
   // webpack: (config) => {
   //   config.resolve.fallback = {
   //     fs: false,
