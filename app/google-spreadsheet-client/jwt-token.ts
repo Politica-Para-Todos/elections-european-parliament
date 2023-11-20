87c329bab51254ff6bd171b6e9589ba167a714db
jopijuco <jpjc@protonmail.com>
2023-11-20 00:03:33 +0000

    Attempt to double check if heroku is injecting vars correctly

--- app/google-spreadsheet-client/jwt-token.ts
+++ app/google-spreadsheet-client/jwt-token.ts
@@ -1,5 +1,8 @@
 import { JWT } from "google-auth-library";
 
+console.log('SERVICE-ACCOUNT-EMAIL: ', process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL![0]);
+console.log('PRIVATE-KEY: ', process.env.GOOGLE_PRIVATE_KEY![1]);
+
 export const jwt = new JWT({
   email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
   key: process.env.GOOGLE_PRIVATE_KEY,
